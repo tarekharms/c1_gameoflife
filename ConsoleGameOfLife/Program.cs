de00941326f3302d5356b5e361f297877af45fde
Tarek Harms <tarek.harms@doepke.de>
2021-10-20 10:33:48 +0200

    Add Spielfeld

---
+++ ConsoleGameOfLife/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using c1_gameoflife.model;
+
+namespace ConsoleGameOfLife
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Spielfeld spielfeld = new Spielfeld(5, 5);
+
+			//*
+			spielfeld.setPunkt(1, 1, 1);
+			spielfeld.setPunkt(2, 1, 1);
+			spielfeld.setPunkt(1, 2, 1);
+			spielfeld.setPunkt(3, 2, 1);
+			spielfeld.setPunkt(2, 2, 1);
+			spielfeld.setPunkt(3, 3, 1);
+			//*/
+			Console.Clear();
+			printSpielfeld(spielfeld);
+
+			Console.ReadLine();
+
+			Console.WriteLine("Anzahl Nachbarn P(4,4): " + spielfeld.getAnzahlNachbarn(4, 4));
+
+			Console.ReadLine();
+			return;
+
+			for (int i = 0; i < 4; i++)
+			{
+				spielfeld.setPunkt(i + 1, i, 1);
+				Console.Clear();
+				printSpielfeld(spielfeld);
+				System.Threading.Thread.Sleep(500);
+			}
+
+			Console.Read();
+		}
+
+		private static void printSpielfeld(Spielfeld spielfeld)
+		{
+			for(int y = 0; y < spielfeld.Hoehe; y++)
+			{
+				for(int x = 0; x < spielfeld.Breite; x++)
+				{
+					Console.Write(spielfeld.getPunkt(x, y) + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ c1_gameoflife/model/Spielfeld.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c1_gameoflife.model
+{
+	public class Spielfeld
+	{
+		private sbyte[,] spielfeld;
+
+		private int breite;
+		public int Breite
+		{
+			get { return this.breite; }
+		}
+
+		public int Hoehe
+		{
+			get { return this.hoehe; }
+		}
+
+		private int hoehe;
+
+		public Spielfeld(int breite, int hoehe)
+		{
+			this.breite = breite;
+			this.hoehe = hoehe;
+
+			this.initialise();
+		}
+
+		private void initialise()
+		{
+			this.spielfeld = new sbyte[this.breite, this.hoehe];
+
+			for(int x = 0; x < this.breite; x++)
+			{
+				for(int y = 0; y < this.breite; y++)
+				{
+					this.spielfeld[x,y] = 0;
+				}
+			}
+		}
+
+		public sbyte getPunkt(int x, int y)
+		{
+			return this.spielfeld[x, y];
+		}
+
+		public void setPunkt(int x, int y, sbyte value)
+		{
+			this.spielfeld[x, y] = value;
+		}
+
+		public int getAnzahlNachbarn(int punktX, int punktY)
+		{
+			int anzahlNachbarn = 0;
+
+			for(int x = punktX - 1; x <= punktX + 1; x++)
+			{
+				if (x < 0 || x >= this.breite)
+					continue;
+
+				for(int y = punktY - 1; y <= punktY + 1; y++)
+				{
+					if (y < 0 || y >= this.hoehe)
+						continue;
+					else if (x == punktX && y == punktY)
+						continue;
+
+					if (this.getPunkt(x, y) == 1)
+						anzahlNachbarn++;
+				}
+			}
+
+			return anzahlNachbarn;
+		}
+	}
+}
