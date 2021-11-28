e81d6e0f0af05e734cb39f4904753a2dd4c45b7e
Tarek Harms <tarek.harms@outlook.de>
2021-11-28 16:33:56 +0100

    Add Interfaces

---
+++ c1_gameoflife/interfaces/IRegeln.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c1_gameoflife.model;
+
+namespace c1_gameoflife.interfaces
+{
+    public interface IRegeln
+    {
+        void regelnAnwendenSpielfeld(Spielfeld spielfeld);
+    }
+}
---
+++ c1_gameoflife/interfaces/ISavegame.cs
@@ -0,0 +1,11 @@
+﻿using c1_gameoflife.model;
+
+namespace c1_gameoflife.interfaces
+{
+    interface ISavegame
+    {
+        Spielfeld load(string filePath);
+
+        void save(Spielfeld spielfeld, string filePath);
+    }
+}
